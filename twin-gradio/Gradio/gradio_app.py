bff311e423b7dd9b904867ef41913a3e4a070644
PrMine54 <yildirimgiray@gmail.com>
2024-07-23 13:37:09 +0300

    Photos and change on the gradio code.

--- twin-gradio/Gradio/gradio_app.py
+++ twin-gradio/Gradio/gradio_app.py
@@ -1,13 +1,17 @@
 import numpy as np
 import gradio as gr
 import pickle
-import joblib
+from pymongo import MongoClient
 
 MODEL_PATH = "knn_model.pkl"
-SCALER_PATH = "scaler_model.pkl"
 
 model = pickle.load(open(MODEL_PATH, 'rb'))
-#scaler = joblib.load(SCALER_PATH)
+
+mongo_client = MongoClient()
+mongo_client = MongoClient("mongodb://admin:password@10.8.129.8:27017/")
+
+db = mongo_client['mydatabase']
+prediction_table = db['predictions']
 
 
 def predict(input1: float,
@@ -17,6 +21,15 @@ def predict(input1: float,
             input5: float):
     input_data = np.array([[input1, input2, input3, input4, input5]])
     prediction = model.predict(input_data)
+    records = {
+        'vib_spindle__quantile__q_0.7': input1,
+        'vib_spindle__quantile__q_0.6': input2,
+        'vib_spindle__fft_coefficient__attr_"abs"__coeff_0': input3,
+        'vib_spindle__fft_coefficient__attr_"real"__coeff_0': input4,
+        'vib_spindle__quantile__q_0.8': input5,
+        'prediction': prediction
+    }
+    records = db.prediction_table.insert(records)
     return "Faulty" if prediction[0] == 1 else "Healthy"
 
 
@@ -30,9 +43,8 @@ iface = gr.Interface(
         gr.Number(label="Feature 5")
     ],
     outputs="text",
-    title="K-NN Binary Classification",
+    title="CNC Machine Predictive Maintenance",
     description="Enter five features to get a binary classification result through K-NN Binary Classification model."
 )
 
-iface.launch()
-#iface.launch(server_name="0.0.0.0", server_port=7860)
+iface.launch(server_name="0.0.0.0", server_port=8000)
